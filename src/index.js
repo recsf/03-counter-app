c612bcd1f269c62e43b61711749cfa772c39797b
recsf <erickgarciagsf@gmail.com>
2022-02-23 18:31:31 -0600

    index-clase1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM  from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const saludo  = <h1>Hola Mundo</h1>;
+
 
 const divRoot = document.querySelector('#app');
 ReactDOM.render(saludo, divRoot);
