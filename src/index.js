04f05d4c56240964c57d693faf1dc7bcd400507e
recsf <erickgarciagsf@gmail.com>
2022-02-28 09:47:01 -0600

    index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-
-const saludo  = <h1>Hola Mundo</h1>;
-
+import CounteraApp from './CounterApp';
+import './index.css';
 
 const divRoot = document.querySelector('#app');
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render(<CounteraApp/> ,divRoot);
